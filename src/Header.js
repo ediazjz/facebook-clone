fd1664cc81a48c269ae7ff104a81c522110ca585
ediazjz <ediazjz@outlook.com>
2021-03-16 13:24:55 -0600

    Add Header Middle Skeleton

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,10 @@
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
 
 const Header = () => { 
   return (
@@ -14,7 +19,24 @@ const Header = () => {
         </div>
       </div>
 
-      <div className="header__middle"></div>
+      <div className="header__middle">
+        <div className="header__option">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <FlagIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SubscriptionsIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <StorefrontIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SupervisedUserCircleIcon fontSize="large" />
+        </div>
+      </div>
+      
       <div className="header__right"></div>
     </div>
   )
