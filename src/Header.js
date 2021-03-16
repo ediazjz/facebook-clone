e49753529775434678343333b23f54e54cc327ca
ediazjz <ediazjz@outlook.com>
2021-03-16 13:19:11 -0600

    Add Header Left Skeleton

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,19 @@
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
 
 const Header = () => { 
   return (
     <div className="header">
-      <div className="header__left"></div>
+      <div className="header__left">
+        <img 
+          src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg"
+          alt="Facebook logo" />
+        <div className="header__input">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+
       <div className="header__middle"></div>
       <div className="header__right"></div>
     </div>
