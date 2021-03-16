4a3a4ec4bbdc868aafc38cae7931d5f8589805d0
ediazjz <ediazjz@outlook.com>
2021-03-16 12:54:21 -0600

    Add Header Component Skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
       <h1>facebook clone</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+const Header = () => { 
+  return (
+    <div className="header">
+      <div className="header__left"></div>
+      <div className="header__middle"></div>
+      <div className="header__right"></div>
+    </div>
+  )
+}
+
+export default Header
