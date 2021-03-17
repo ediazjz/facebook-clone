924e4a6ad9289223632ed6bd9c6a3f9fd131469e
ediazjz <ediazjz@outlook.com>
2021-03-16 23:53:41 -0600

    Add Styles to Post Component

--- src/Feed.js
+++ src/Feed.js
@@ -13,7 +13,14 @@ const Feed = () => {
         profilePic="https://i.pravatar.cc/300"
         username="Edgar Díaz"
         timestamp="A few seconds ago"
-        message="Hello world!" />
+        message="Hello world!"
+        image="https://www.newzealand.com/assets/Tourism-NZ/Christchurch-Canterbury/8bb86abcfd/img-1536307813-4242-957-p-C4D67668-0642-F5C5-BC3A684C8BB1F331-2544003__aWxvdmVrZWxseQo_FocalPointCropWzI0MCw0ODAsNTAsNTMsNzUsImpwZyIsNjUsMi41XQ.jpg" />
+      <Post 
+        profilePic="https://i.pravatar.cc/300"
+        username="Edgar Díaz"
+        timestamp="A few seconds ago"
+        message="Hello world!"
+        image="https://www.newzealand.com/assets/Tourism-NZ/Christchurch-Canterbury/8bb86abcfd/img-1536307813-4242-957-p-C4D67668-0642-F5C5-BC3A684C8BB1F331-2544003__aWxvdmVrZWxseQo_FocalPointCropWzI0MCw0ODAsNTAsNTMsNzUsImpwZyIsNjUsMi41XQ.jpg" />
     </div>
   )
 }
--- src/Post.js
+++ src/Post.js
@@ -1,12 +1,15 @@
 import { Avatar } from '@material-ui/core';
 import './Post.css';
+import ThumbUpOutlinedIcon from '@material-ui/icons/ThumbUpOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@material-ui/icons/ChatBubbleOutlineOutlined';
+import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
 
 const Post = ({ profilePic, username, timestamp, message, image }) => {
   return (
     <div className="post">
-      <div className="post__top">
+      <div className="post__header">
         <Avatar src={profilePic} />
-        <div className="post__topInfo">
+        <div className="post__headerInfo">
           <span className="post__username">{username}</span>
           <span className="post__timestamp">{timestamp}</span>
         </div>
@@ -15,6 +18,21 @@ const Post = ({ profilePic, username, timestamp, message, image }) => {
       <div className="post__content">
         <p>{message}</p>
         {image && <img src={image} alt=""/>}
+      </div>
+
+      <div className="post__options">
+        <div className="post__option">
+          <ThumbUpOutlinedIcon />
+          <span>Like</span>
+        </div>
+        <div className="post__option">
+          <ChatBubbleOutlineOutlinedIcon />
+          <span>Comment</span>
+        </div>
+        <div className="post__option">
+          <ShareOutlinedIcon />
+          <span>Share</span>
+        </div>
       </div>
     </div>
   )
