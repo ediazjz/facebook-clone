ab808c40fa3bbf438342174a23e8a5a2b21022fd
ediazjz <ediazjz@outlook.com>
2021-03-16 18:46:47 -0600

    Fix App Styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Feed from './Feed';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
@@ -9,6 +10,8 @@ function App() {
 
       <div className="app__body">
         <Sidebar />
+        
+        <Feed />
       </div>
     </div>
   );
