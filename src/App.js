34b3f04229ad8ccb347216429de01fda3c72692b
ediazjz <ediazjz@outlook.com>
2021-03-17 07:30:15 -0600

    Add Login Componenet Skeleton with Styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import './App.css';
 import Feed from './Feed';
 import Header from './Header';
+import Login from './Login';
 import Sidebar from './Sidebar';
 import Widgets from './Widgets';
 
 function App() {
+  const user = '';
+
   return (
     <div className="app">
-      <Header />
+      {!user ? <Login /> : (
+        <>
+          <Header />
 
-      <div className="app__body">
-        <Sidebar />
-        
-        <Feed />
+          <div className="app__body">
+            <Sidebar />
+            
+            <Feed />
 
-        <Widgets />
-      </div>
+            <Widgets />
+          </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,25 @@
+import { Button } from '@material-ui/core';
+import './Login.css';
+
+const Login = () => {
+  const signIn = () => {
+
+  }
+  return (
+    <div className="login">
+      <div className="login__logo">
+        <img 
+          src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg"
+          alt="Facebook logo" />
+        <img 
+          src="https://www.logo.wine/a/logo/Facebook/Facebook-Logo.wine.svg"
+          alt="Facebook logo text" />
+      </div>
+      <Button type="submit" onClick={signIn}>
+        Sign In  
+      </Button>
+    </div>
+  )
+}
+
+export default Login
