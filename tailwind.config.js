5d363b4aba996ffcb6916684ae886815df468857
ediazjz <ediazjz@outlook.com>
2021-03-20 09:46:24 -0600

    Refactor App

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          "150": "#f1f2f5",
+        }
+      }
+    },
   },
   variants: {
     extend: {},
