0bf02e616ca52673f7ac1108ef1d8ce60e8898f3
ediazjz <ediazjz@outlook.com>
2021-03-20 18:31:48 -0600

    Refactor Post

--- src/Post.js
+++ src/Post.js
@@ -6,21 +6,23 @@ import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
 
 const Post = ({ profilePic, username, timestamp, message, image }) => {
   return (
-    <div className="post">
-      <div className="post__header">
-        <Avatar src={profilePic} />
-        <div className="post__headerInfo">
-          <span className="post__username">{username}</span>
-          <span className="post__timestamp">{new Date(timestamp?.toDate()).toUTCString()}</span>
+    <div className="post w-full mt-4 bg-white rounded-2xl">
+      <div className="post__header mb-3 pt-3 px-4 flex items-center relative">
+        <Avatar className="mr-2" src={profilePic} />
+        <div className="post__headerInfo flex flex-col">
+          <span className="post__username mb-1 font-medium">{username}</span>
+          {/* sm for desktop */}
+          <span className="post__timestamp text-xs text-gray-500">{new Date(timestamp?.toDate()).toUTCString()}</span>
         </div>
       </div>
 
       <div className="post__content">
-        <p>{message}</p>
-        {image && <img src={image} alt=""/>}
+        {/* pb-4 for desktop */}
+        <p className="pt-1 pb-2 px-4">{message}</p>
+        {image && <img className="w-full" src={image} alt=""/>}
       </div>
 
-      <div className="post__options">
+      <div className={`post__options mb-1 mx-4 flex justify-evenly text-gray-500 ${!image && 'border-t'} border-gray-300`}>
         <div className="post__option">
           <ThumbUpOutlinedIcon />
           <span>Like</span>
