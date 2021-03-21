af35baa011265c4707f5a0620b1092d9fe136bd3
ediazjz <ediazjz@outlook.com>
2021-03-20 20:01:39 -0600

    HOTFIX - Tailwind CSS Layer Purging
    In production, the component layer was being purged, so it purged all classes with the child combinator
    This might be refactored in the future to optimize production by removing Material UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    layers: ['base', 'utilities'],
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html']
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
